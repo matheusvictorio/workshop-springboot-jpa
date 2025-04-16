78fce401e949e33b8033b0da1a6f5a9add0285cb
matheusvictorio <matheusvic2016@gmail.com>
2025-04-16 18:21:51 -0300

    Project created

---
+++ src/test/java/com/praticando/courseSpring/CourseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praticando.courseSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
