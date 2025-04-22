c0906d4328e784e70ad4de067931c1787edb603b
matheusvictorio <matheusvic2016@gmail.com>
2025-04-22 15:49:20 -0300

    User update

--- src/main/java/com/praticando/courseSpring/resources/UserResource.java
+++ src/main/java/com/praticando/courseSpring/resources/UserResource.java
@@ -43,4 +43,10 @@ public class UserResource {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@org.springframework.web.bind.annotation.PathVariable Long id, @RequestBody User user) {
+        user = userService.update(id, user);
+        return ResponseEntity.ok().body(user);
+    }
 }
