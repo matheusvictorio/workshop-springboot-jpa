e2ae5b8375df2eda367c4d2a702056105a2694ed
matheusvictorio <matheusvic2016@gmail.com>
2025-04-22 12:36:49 -0300

    Payment, one-to-one association

--- src/main/java/com/praticando/courseSpring/entities/Order.java
+++ src/main/java/com/praticando/courseSpring/entities/Order.java
@@ -28,6 +28,9 @@ public class Order implements java.io.Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new java.util.HashSet<>();
 
+    @OneToOne (mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order(){
     }
 
@@ -70,6 +73,14 @@ public class Order implements java.io.Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
